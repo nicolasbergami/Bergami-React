cf340a7d580f48be1e6cbf36af6ac9efa45d21e9
nicolasbergami <nicolasbergami2013@gmail.com>
2022-08-22 18:50:23 -0300

    Agregado de components NavBar con react-bootstrap y mdb-react-ui-kit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBadge
+} from 'mdb-react-ui-kit';
 export function NavBar(){
 
 return (
@@ -12,21 +19,16 @@ return (
     <Container>
       <Navbar.Brand href="#home">Bici Mundo</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
+      <Navbar.Collapse id="basic-navbar-nav " className="justify-content-end">
+        <Nav className="justify-content-center">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
+          <Nav.Link href="#link"> 
+          <MDBBadge pill color='danger'>!</MDBBadge>
+              <span>
+                <MDBIcon fas icon='shopping-cart'></MDBIcon>
+              </span> 🛒</Nav.Link>
+          
         </Nav>
       </Navbar.Collapse>
     </Container>
